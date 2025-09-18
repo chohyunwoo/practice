7fe34a6402aae7063cecfd22719eabe8190250a4
조현우 <gusdndlek12@naver.com>
2025-09-18 16:07:06 +0900

    baekjoon 10699

---
+++ src/baekjoon/Time10699.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time10699 {
+    public static void main(String[] args) {
+
+        //현재 날짜 객체 생성
+        LocalDateTime now = LocalDateTime.now();
+        //출력형식 정의
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        //Local 객체를 formatter 이용해서 문자열 변환
+        System.out.println(now.format(dtf));
+    }
+}
